7814e7d2eedbe93cba6563376d1de86bc16ce50f
Joey <gallottaj@hotmail.com>
2020-05-02 10:58:22 -0700

    Add project files.

---
+++ Exercise10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file; 
+
+            if (args.Length == 0)
+            {
+                file = File.ReadAllText(@"../../teaTimeFile.txt");
+            }
+            else
+            {
+                file = File.ReadAllText(args[0]);
+            }
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            //checks for pattern
+            //match if string ends in e or t
+            Int64 matchCount = Regex.Matches(file, "([te])(?![a-z])", RegexOptions.Multiline | RegexOptions.IgnoreCase).Count;
+
+            //display results to console
+            Console.WriteLine("There are " + matchCount + " words that end in t or e");
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
